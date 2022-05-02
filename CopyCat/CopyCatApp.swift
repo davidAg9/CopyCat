fdd559330125b812db495b5681daae3e86ce4f39
davidAg9 <shane_alpha9@outlook.com>
2022-05-02 12:32:17 +0000

    Initial Commit

---
+++ CopyCat/CopyCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyCatApp.swift
+//  CopyCat
+//
+//  Created by Dave Agyakwa on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CopyCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
